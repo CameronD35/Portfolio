84c039632b07a2edcc4d36db6ff4a89ad714ba8d
CameronD35 <camster09@ca.rr.com>
2026-02-01 15:28:23 -0800

    Added stuff to the graphic design section. Testing out some designs rn.

---
+++ src/scripts/themeManage.js
@@ -0,0 +1,30 @@
+function setThemeAssetScroll() {
+
+    const cardWidth = document.querySelector(".card").clientWidth;
+
+
+}
+
+// function setFooterColor() {
+
+//     const currentPath = window.location.pathname;
+
+//     console.log(currentPath, currentPath === "/")
+
+//     if (currentPath === "/") {
+
+//         document.documentElement.style.setProperty("--footer-color", "var(--custom-dark-brown)");
+//         document.documentElement.style.setProperty("--footer-accent", "var(--custom-tan)");
+//         document.documentElement.style.setProperty("--footer-accent-hover", "var(--custom-white)");
+
+//     } else {
+
+//         document.documentElement.style.setProperty("--footer-color", "var(--custom-white)");
+//         document.documentElement.style.setProperty("--footer-accent", "var(--custom-black-st)");
+//         document.documentElement.style.setProperty("--footer-accent-hover", "var(--custom-black)");
+
+//     }
+
+// }
+
+export {setFooterFixing};
