f5ac5972073ee98f32b3c43978c7eca42a031f8f
CameronD35 <camster09@ca.rr.com>
2026-02-01 22:46:31 -0800

    Added more stuff to graphic design section.

--- src/scripts/themeManage.js
+++ src/scripts/themeManage.js
@@ -1,6 +1,33 @@
 function setThemeAssetScroll() {
 
-    const cardWidth = document.querySelector(".card").clientWidth;
+    const cards = document.querySelectorAll(".card");
+
+    cards.forEach((card) => {
+
+        const assetContainer = card.querySelector(".asset-container");
+        const leftShadow = card.querySelector(".left-shadow");
+        const rightShadow = card.querySelector(".right-shadow");
+
+        console.log(card);
+
+        assetContainer.addEventListener("scroll", (evt) => {
+
+            // console.log(leftShadow);
+
+            const maxScroll = assetContainer.scrollWidth - assetContainer.clientWidth;
+            const scrollDistance = assetContainer.scrollLeft;
+
+            console.log(assetContainer.scrollLeft, maxScroll);
+
+            if (scrollDistance > 0) { leftShadow.style.opacity = 1; }
+            else { leftShadow.style.opacity = 0; }
+
+            if (scrollDistance < maxScroll) { rightShadow.style.opacity = 1; }
+            else { rightShadow.style.opacity = 0; }
+
+        })
+
+    })
 
 
 }
@@ -27,4 +54,4 @@ function setThemeAssetScroll() {
 
 // }
 
-export {setFooterFixing};
+export {setThemeAssetScroll};
