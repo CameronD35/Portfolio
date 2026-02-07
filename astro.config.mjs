45e35bc9f45cd38d86fc451aa7ea73654f72c92e
CameronD35 <camster09@ca.rr.com>
2026-02-06 23:23:31 -0800

    Did a first entry on my third blog.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from 'astro/config';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
+import rehypePrettyCode from 'rehype-pretty-code';
 import mdx from '@astrojs/mdx';
 
 import icon from 'astro-icon';
@@ -11,13 +12,14 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   integrations: [icon(), mdx({
-
+    syntaxHighlight: false,
     remarkPlugins: [remarkMath],
-    rehypePlugins: [rehypeKatex]
+    rehypePlugins: [rehypeKatex, [rehypePrettyCode, { theme: 'github-dark' }]]
 
   }), react()],
   markdown: {
+    syntaxHighlight: false,
     remarkPlugins: [remarkMath],
-    rehypePlugins: [rehypeKatex]
+    rehypePlugins: [rehypeKatex, [rehypePrettyCode, { theme: 'github-dark' }]],
   }
 });
