9463a717acdbf2d04384c5427b2f1066c9c956ff
CameronD35 <camster09@ca.rr.com>
2026-02-16 00:12:42 -0800

    Got the pdf files >:].

---
+++ src/components/PDFViewer.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useRef, useState } from 'react';
+import * as pdfjs from 'pdfjs-dist';
+pdfjs.GlobalWorkerOptions.workerSrc = window.location.origin + '/pdf.worker.min.mjs';
+
+const PDFViewer = ({url}) => {
+    const canvasRef = useRef(null);
+
+    const fileName = url.substring(url.lastIndexOf('/') + 1);
+
+    const [width, setWidth] = useState(null);
+    const [height, setHeight] = useState(null);
+
+    useEffect(() => {
+
+        const renderPDF = async () => {
+
+            const loadingTask = pdfjs.getDocument(url);
+            const pdf = await loadingTask.promise;
+            const page = await pdf.getPage(1);
+            const viewport = page.getViewport({scale: 2});
+            const canvas = canvasRef.current;
+            const context = canvas.getContext('2d');
+            console.log(viewport);
+            setHeight(viewport.height);
+            setWidth(viewport.width);
+            canvas.height = viewport.height;
+            canvas.width = viewport.width;
+
+            const renderContext = {
+
+                canvasContext: context,
+                viewport: viewport
+
+            };
+
+            await page.render(renderContext).promise;
+
+        };
+
+        renderPDF();
+
+    }, [url]);
+
+    return <div className="pdf-viewer" style={{height: height / 5}}>
+                <div className="download"></div>
+                <span className="fileName">{fileName}</span><canvas style={{width: width / 5, height: height / 5}}data-name={fileName} ref={canvasRef}/>
+           </div>;
+
+};
+
+export default PDFViewer;
