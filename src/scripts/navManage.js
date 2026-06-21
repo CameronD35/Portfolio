556bda20a3ed6755dbeb80886f65c07d49cd42c7
CameronD35 <camster09@ca.rr.com>
2026-06-21 16:30:57 -0400

    Nav animation complete.

--- src/scripts/navManage.js
+++ src/scripts/navManage.js
@@ -12,25 +12,63 @@ function appendDropdown() {
 
 }
 
-function hideNavElements() {
+function toggleNavElements(evt) {
+
+    const windowWidth = evt.target.innerWidth;
 
     const components = document.querySelectorAll(`.nav-component[data-nav="true"]`);
 
-    const xFinal = components[3].querySelector("span").getBoundingClientRect()["right"];
+    const hidden = (components[0].getAttribute("data-compressed") === "true");
 
-    components.forEach((elem, i) => {
-        // console.log(elem);
+    if (hidden && windowWidth > 650) {
 
-        const xInitial = elem.querySelector("span").getBoundingClientRect()["right"];
+        components.forEach((elem, i) => {
 
-        console.log(xFinal - xInitial);
+            elem.setAttribute("data-compressed", "false");
 
-        const time = 1000 - ((i + 1) * 125);
-        const distance = 600 - ((i + 1) * 150);
+            const time = 1000 - ((i + 1) * 125);
 
-        // elem.animate([{transform: `translateX(${distance}px)`}], {duration: time, fill: "forwards", delay: 1000, easing: "cubic-bezier(0.175, 0.885, 0.32, 1.0)"})
-    });
+            elem.animate([{display: 'inline'}], {duration: 0, fill: "forwards"})
+            .finished.then(() => {
+
+                return (() => {
+
+                    elem.animate([{transform: `translateX(0px)`, opacity: 1}], {duration: time, fill: "forwards", easing: "cubic-bezier(0.175, 0.885, 0.32, 1.0)"});
+
+                })()
+
+            });
+
+        });
+
+    } else if (!hidden && windowWidth < 650) {
+
+        const xFinal = components[3].querySelector("span").getBoundingClientRect()["right"];
+
+        components.forEach((elem, i) => {
+
+            elem.setAttribute("data-compressed", "true");
+
+            const xInitial = elem.querySelector("span").getBoundingClientRect()["right"];
+
+            const time = 1000 - ((i + 1) * 125);
+            const distance = xFinal - xInitial;
+
+            elem.animate([{transform: `translateX(${distance}px)`, opacity: 0}], {duration: time, fill: "forwards", easing: "cubic-bezier(0.175, 0.885, 0.32, 1.0)"})
+            .finished.then(() => {
+
+                return (() => {
+
+                    elem.animate([{display: 'none'}], {duration: 0, fill: "forwards"});
+
+                })()
+
+            });
+
+        });
+
+    }
 
 }
 
-export {appendDropdown, hideNavElements}
+export {appendDropdown, toggleNavElements}
